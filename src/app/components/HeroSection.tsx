8b54b005f1af851ba35dd6068083f273110ed683
danishRajawat <ibnfarooq070@gmail.com>
2026-03-04 10:19:11 +0530

    hero section slider fixed

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
-
 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 const slides = [
@@ -18,41 +17,50 @@ const slides = [
   "/heroImage8.jpeg",
 ];
 export function HeroSection() {
-
   return (
-<section id="home" className="relative w-full h-[45vh] sm:h-[70vh] lg:h-[85vh]">
+    <section
+      id="home"
+      className="relative w-full h-[45vh] sm:h-[70vh] lg:h-[85vh]"
+    >
+      {/* Navigation Buttons */}
+      <button
+        className="swiper-button-prev-custom hidden md:flex absolute left-6 top-1/2 -translate-y-1/2 z-10
+  bg-white/20 backdrop-blur-md border border-white/30 text-white p-4 rounded-full shadow-lg hover:scale-110 transition-all"
+      >
+        <ChevronLeft size={28} className="text-black" />
+      </button>
 
-  {/* Navigation Buttons */}
-  <button className="swiper-button-prev-custom hidden md:flex absolute left-6 top-1/2 -translate-y-1/2 z-10
-  bg-white/20 backdrop-blur-md border border-white/30 text-white p-4 rounded-full shadow-lg hover:scale-110 transition-all">
-    <ChevronLeft size={28} className='text-black'/>
-  </button>
+      <button
+        className="swiper-button-next-custom hidden md:flex absolute right-6 top-1/2 -translate-y-1/2 z-10
+  bg-white/20 backdrop-blur-md border border-white/30 text-white p-4 rounded-full shadow-lg hover:scale-110 transition-all"
+      >
+        <ChevronRight className="text-black" size={28} />
+      </button>
 
-  <button className="swiper-button-next-custom hidden md:flex absolute right-6 top-1/2 -translate-y-1/2 z-10
-  bg-white/20 backdrop-blur-md border border-white/30 text-white p-4 rounded-full shadow-lg hover:scale-110 transition-all">
-    <ChevronRight className='text-black'  size={28} />
-  </button>
-
-  <Swiper
-    modules={[Navigation]}
-    navigation={{
-      nextEl: ".swiper-button-next-custom",
-      prevEl: ".swiper-button-prev-custom",
-    }}
-    loop={true}
-    className="w-full h-full"
-  >
-    {slides.map((img, index) => (
-      <SwiperSlide key={index}>
-        <img
-          src={img}
-          alt="Defence Training"
-          className="w-full h-full  lg:object-fill object-contain"
-        />
-      </SwiperSlide>
-    ))}
-  </Swiper>
-
-</section>
+      <Swiper
+        modules={[Navigation , Autoplay]}
+        navigation={{
+          nextEl: ".swiper-button-next-custom",
+          prevEl: ".swiper-button-prev-custom",
+        }}
+        loop={true}
+        spaceBetween={30}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        className="w-full h-full"
+      >
+        {slides.map((img, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={img}
+              alt="Defence Training"
+              className="w-full h-full   lg:object-fill object-contain"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 }
